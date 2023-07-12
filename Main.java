4d761cd5aa49ae5a82283bcb7347f1992d37783f
Oldrick Weenink <o.weenink@hotmail.com>
2023-07-12 14:24:23 +0200

    Initial commit for v2

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // Version 2
         CalculatorModel theModel = new CalculatorModel();
         CalculatorView theView = new CalculatorView();
         CalculatorControl theController = new CalculatorControl(theView, theModel);
